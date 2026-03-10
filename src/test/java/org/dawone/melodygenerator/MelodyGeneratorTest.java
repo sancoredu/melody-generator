17fcb75d9e42c4ffc280d283934c38ee7158cf3d
ccorreas <correasberenguer@gmail.com>
2026-03-10 19:58:51 +0100

    feat: Tests para compases

--- src/test/java/org/dawone/melodygenerator/MelodyGeneratorTest.java
+++ src/test/java/org/dawone/melodygenerator/MelodyGeneratorTest.java
@@ -2,9 +2,31 @@ package org.dawone.melodygenerator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class MelodyGeneratorTest {
 
     @Test
-    void generateMelody() {
+    void generateMelody_SiCompasesVale0_DevuelveException() {
+        MelodyGenerator melodyGenerator = new MelodyGenerator(new Random());
+        assertThrows(IllegalArgumentException.class, () ->   melodyGenerator.generateMelody(0));
     }
+
+    @Test
+    void generateMelody_SiCompasesVale8_DevuelveException(){
+        MelodyGenerator melodyGenerator = new MelodyGenerator(new Random());
+        assertThrows(IllegalArgumentException.class, () ->   melodyGenerator.generateMelody(8));
+    }
+
+    @Test
+    void generateMelody_SiCompasesVale1_DevuelveCompas(){
+        String expected = "";
+        MelodyGenerator melodyGenerator = new MelodyGenerator(new Random());
+        assertEquals(expected, melodyGenerator.generateMelody(1));
+    }
+
+
 }
